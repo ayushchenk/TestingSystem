3fedcac1954927834bc28bee6f3424123dbcc6c9
Vladislav Lapidus <lapidusvlad@gmail.com>
2019-04-09 18:51:41 +0300

    Adding View, optimizing code on controller.

--- TestingSystem.Web/Controllers/HomeController.cs
+++ TestingSystem.Web/Controllers/HomeController.cs
@@ -8,7 +8,10 @@ namespace TestingSystem.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index() => View();
+        public ActionResult Index()
+        {
+            return View();
+        }
 
         public ActionResult About() => View();
 
