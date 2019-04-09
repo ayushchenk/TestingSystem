631f95cf2da3a3e80e981808e3def97dbd6ba8f2
Vladislav Lapidus <lapidusvlad@gmail.com>
2019-04-09 17:50:54 +0300

    Optimization and packaging of the code in the home controller and adding design.

--- TestingSystem.Web/Controllers/HomeController.cs
+++ TestingSystem.Web/Controllers/HomeController.cs
@@ -8,23 +8,10 @@ namespace TestingSystem.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        public ActionResult Index() => View();
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        public ActionResult About() => View();
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        public ActionResult Contact() => View();
     }
 }
