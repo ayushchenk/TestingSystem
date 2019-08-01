36d67bbf8ac7ad31e9bba52d8252756d3beb5306
Aleksandr <a.yushchenk@gmail.com>
2019-08-01 16:22:35 +0300

    Add StudyingMaterial api controller

---
+++ TestingSystem.Web/ApiControllers/StudyingMaterialsApiController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using TestingSystem.BOL.Model;
+using TestingSystem.BOL.Service;
+
+namespace TestingSystem.Web.ApiControllers
+{
+    public class StudyingMaterialsApiController : ApiController
+    {
+        private IEntityService<StudyingMaterialDTO> materialService;
+
+        public StudyingMaterialsApiController(IEntityService<StudyingMaterialDTO> materialService)
+        {
+            this.materialService = materialService;
+        }
+
+        // GET: api/SubjectApi
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok(await materialService.GetAllAsync());
+        }
+    }
+}
