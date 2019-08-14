52628b70c8ea56d44d43a66000c49253b491a7a4
Aleksandr <a.yushchenk@gmail.com>
2019-08-14 10:23:07 +0300

    Add subject api controller

---
+++ TestingSystem.Web/ApiControllers/SubjectApiController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using TestingSystem.BOL.Model;
+using TestingSystem.BOL.Service;
+
+namespace TestingSystem.Web.ApiControllers
+{
+    public class SubjectApiController : ApiController
+    {
+        private IEntityService<StudentDTO> subjectService;
+
+        public SubjectApiController(IEntityService<StudentDTO> subjectService)
+        {
+            this.subjectService = subjectService;
+        }
+
+        // GET: api/SubjectApi
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok(await subjectService.GetAllAsync());
+        }
+
+        // GET: api/SubjectApi/5
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            return Ok(await subjectService.GetAsync(id));
+        }
+    }
+}
