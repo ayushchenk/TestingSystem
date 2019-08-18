c3bfcee64621c4760fecbd942c03011cd39daa4b
Vladislav Lapidus <it.lapidus@gmail.com>
2019-08-18 17:53:08 +0300

    The new principle of working on errors 404 and 500.

--- TestingSystem.Web/Controllers/ErrorController.cs
+++ TestingSystem.Web/Controllers/ErrorController.cs
@@ -10,11 +10,13 @@ namespace TestingSystem.Web.Controllers
     {
         public ActionResult _500()
         {
+            Response.StatusCode = 500;
             return View();
         }
 
         public ActionResult _404()
         {
+            Response.StatusCode = 404;
             return View();
         }
     }
