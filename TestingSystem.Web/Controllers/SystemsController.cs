ed2eff98f03605fbafbab76ab9599d58748630bf
Vladislav Lapidus <it.lapidus@gmail.com>
2019-08-26 14:01:23 +0300

    Home pages, index & emp.

---
+++ TestingSystem.Web/Controllers/SystemsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestingSystem.Web.Controllers
+{
+    public class SystemsController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index(string name = "")
+        {
+            ViewEngineResult result = ViewEngines.Engines.FindView(ControllerContext, name, null);
+            if (result == null || result.View == null)
+            {
+                Response.StatusCode = 404;
+            }
+            return View(name);
+        }
+    }
+}
